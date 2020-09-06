636c121fa5fb06fa22fc5bb8e54bea14518bf912
anapotje <anapotje@gmail.com>
2020-09-06 15:41:17 +0100

    Implement exercise 002 - isFromManchester

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -2,8 +2,13 @@ package com.techreturners.exercise002;
 
 public class Exercise002 {
 
-    public boolean isFromManchester(/* Person person */) {
-        return false;
+    public boolean isFromManchester(Person person1) {
+
+        if (person1.getCity() == "Manchester") {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public boolean canWatchFilm(/* Person person, int ageLimit*/) {
